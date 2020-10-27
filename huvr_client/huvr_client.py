1d9d8ee2d031dbc5da4faa5de974aca3c763f1b2
Jeff Sheffield <jeff.sheffield@gmail.com>
2020-10-27 14:30:08 -0500

    'upload_project_media()' return response #2768

--- huvr_client/huvr_client.py
+++ huvr_client/huvr_client.py
@@ -210,6 +210,7 @@ class Client(object):
                     print(response.status_code)
                     print(response.reason)
                 return (response.status_code, response.text)
+        return (response.status_code, response.text)
 
     def profiles_name_lookup(self, profile_name):
         """ This wraps the project_types query and search'es for a name string """
