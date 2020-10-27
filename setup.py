2f36d646b4e1661653d881b5e8619b85010c6cb9
Scott White <sww314@gmail.com>
2020-10-27 11:53:17 -0500

    dist: update version number 0.2.3 was bad.

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = []
@@ -19,30 +19,30 @@ test_requirements = []
 
 setup(
     author="HUVRdata",
-    author_email='api@huvrdata.com',
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    author_email="api@huvrdata.com",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     description="A python client for the HUVRdata API.",
     install_requires=requirements,
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='huvr_client',
-    name='huvr_client',
-    packages=find_packages(include=['huvr_client', 'huvr_client.*']),
+    keywords="huvr_client",
+    name="huvr_client",
+    packages=find_packages(include=["huvr_client", "huvr_client.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/huvrdata/huvr-client',
-    version='0.2.3',
+    url="https://github.com/huvrdata/huvr-client",
+    version="0.2.4",
     zip_safe=False,
 )
