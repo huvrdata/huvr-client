48f87959bd019906c42f0cf183107987945ad798
David Wolgemuth <davidjwolgemuth@gmail.com>
2023-09-21 15:52:49 -0400

    requirements in setup

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ from setuptools import setup, find_packages
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+requirements = ["requests"]
 
 setup(
     author="HUVRdata",
